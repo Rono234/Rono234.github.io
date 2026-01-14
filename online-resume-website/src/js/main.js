778a505e030fb20b915f2bd618009ebbc14fcd53
Raneen AlRammahi <raneenhaider4@gmail.com>
2026-01-14 00:06:22 -0500

    Initial commit: Resume project

---
+++ online-resume-website/src/js/main.js
@@ -0,0 +1,14 @@
+// const header = require('../components/header');
+// const footer = require('../components/footer');
+// const resume = require('../components/resume');
+
+// function renderResume() {
+//     const app = document.getElementById('app');
+//     app.innerHTML = '';
+//     app.appendChild(header());
+//     app.appendChild(resume());
+//     app.appendChild(footer());
+// }
+
+// document.addEventListener('DOMContentLoaded', renderResume);
+
